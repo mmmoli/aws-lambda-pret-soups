d7876da7aa107552a59a54b01da5e81f288a97fd
Michele Memoli <michele@100shapes.com>
2017-01-13 19:06:53 +0000

    Added fake endpoint to test promises

--- src/api.js
+++ src/api.js
@@ -1,9 +1,18 @@
 import API from 'claudia-api-builder'
+import fs from 'fs'
+import denodeify from 'denodeify'
 // import soups from './soups';
 
 const api = new API()
 
 api.get('/', () => 'Hello')
+
+api.get('/packagejson', () => {
+	const read = denodeify(fs.readFile);
+	return read('./package.json')
+		.then(JSON.parse)
+		.then(val => val);
+});
 
 // api.get('/soups', () => soups());
 
